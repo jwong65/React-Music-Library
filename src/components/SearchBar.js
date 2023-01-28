6b83e56b0b0b0323db6bacc254e3a5fa3b256d4d
jwong65 <jason.wong6507@gmail.com>
2023-01-27 19:33:52 -0500

    Fixed the useRef syntax error so it was not an array being defined

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import { SearchContext } from '../context/SearchContext'
 
 
 export default function SearchBar() {
-let [input, handleInput] = useContext(SearchContext)
+let {input, handleInput} = useContext(SearchContext)
 // Destructured it to put it as props
 
 
@@ -12,7 +12,7 @@ let [input, handleInput] = useContext(SearchContext)
         <input ref={input} type='text' placeholder='Search Term here' 
         />
         {/* onChange={(e)=>{setInput(e.target.value)}} is not used because useContext provides the reference to handleSearch */}
-        <button onClick={(e)=>handleInput(e)}>Submit Button</button>
+        <button onClick={(e)=>handleInput(e, input.current.value)}>Submit Button</button>
     </form>
   )
 }
