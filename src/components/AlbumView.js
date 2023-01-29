4b8225b5e71cceb2957f70728b5d83812fe3c25c
jwong65 <jason.wong6507@gmail.com>
2023-01-29 13:34:55 -0500

    Added two views for Artist and Album

---
+++ src/components/AlbumView.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react'
+
+export default function AlbumView() {
+    const [albumData, setAlbumData] = useState([])
+  return (
+    <div>AlbumView</div>
+  )
+}
---
+++ src/components/ArtistView.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react'
+
+export default function ArtistView() {
+    const [artistData, setArtistData] = useState([])
+  return (
+    <div>ArtistView</div>
+  )
+}
